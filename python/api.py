2c4e55fb8678938fba58c82221c05a8d31e61d14
mdarby85 <mattdarby1985@gmail.com>
2019-11-15 20:23:21 -0600

    Refactored the python code and implemented basic handlers and routing

---
+++ python/api.py
@@ -0,0 +1,52 @@
+from webob import Request, Response
+from wsgiref.simple_server import make_server
+
+# Main class and functions where we define everything
+# for our custom API implementation
+class API:
+
+    def __init__(self):
+        self.routes = {}
+
+    # Override the API class __call__ method
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+
+        response = self.handle_request(request)
+
+        return response(environ, start_response)
+
+    # This function is the default call when 
+    # pages that do not exist are accessed
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = str(response.status_code) + " Page Not found."
+
+    # Main handler for handling path requests
+    # Calls find_handler() to get path and 
+    # handles it accordinly
+    def handle_request(self, request):
+        response = Response()
+
+        handler = self.find_handler(request_path=request.path)
+
+        # If handler is valid
+        if handler is not None:
+            handler(request, response)
+        # Otherwise, return the default response
+        else:
+            self.default_response(response)
+        return response
+
+    # Checks if request_path is a valid route
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            if path == request_path:
+                return handler
+
+    # Returns the path
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        return wrapper
--- python/app.py
+++ python/app.py
@@ -1,31 +1,22 @@
-import os
+from api import API
 from wsgiref.simple_server import make_server
 
-def web_app(environment, response):
+# Main file that runs the entire app
+app = API()
 
-    # Get File Contents
-    file_contents = b""
-    with open("./templates/index.html", "rb") as file:
-        file_contents = file.read()
+# Route for '/home' path
+@app.route("/home")  # Flask-style annotation
+def home(request, response):
+    response.text = "Hello from the HOME page"
 
-    # Add Dynamic Content
-    response_body = b"This is a header!".join(
-        b"".join(
-            file_contents.split(b"%HEAD)")
-        ).split(b"%(HEADING)")
-    )
+# Route for '/success' path
+@app.route("/success")  # Flask-style annotation
+def success(request, response):
+    response.text = "Hello from the SUCCESS page"
 
-    # Heading
-    status = '200 OK'
-    headers = [
-        ('Content-type', 'text/html; charset=utf-8'), ('Content-Length', str(len(response_body)))
-        ]
+# Test server for running the Python code
+httpd = make_server('localhost', 8000, app)
+    # print("Serving on port 8000...\nVisit http://127.0.0.1:8000\nTo kill the server we enter 'control + c'")
 
-    # Send Response
-    response(status, headers)
-    return [response_body]
-
-with make_server('', 8000, web_app) as server:
-    print("Serving on port 8000...\nVisit http://127.0.0.1:8000\nTo kill the server we enter 'control + c'")
-
-    server.serve_forever()
+# Wait for a single request, serve it and quit
+httpd.serve_forever()
