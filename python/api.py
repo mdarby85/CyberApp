d763541e78b1bbf9e645ae92e70bce83b9856fe6
mdarby85 <mattdarby1985@gmail.com>
2019-11-22 22:18:21 -0600

    Changes made to fix post requests and retrieval

--- python/api.py
+++ python/api.py
@@ -1,5 +1,7 @@
 from webob import Request, Response
+import hashlib
 from wsgiref.simple_server import make_server
+
 
 # Main class and functions where we define everything
 # for our custom API implementation
@@ -11,6 +13,7 @@ class API:
     # Override the API class __call__ method
     def __call__(self, environ, start_response):
         request = Request(environ)
+        # print(request) # prints request method i.e. GET, POST, etc
 
         response = self.handle_request(request)
 
@@ -20,11 +23,11 @@ class API:
     # pages that do not exist are accessed
     def default_response(self, response):
         response.status_code = 404
-        response.text = str(response.status_code) + " Page Not found."
+        response.text = str(response.status_code) + " Page Not found!"
 
     # Main handler for handling path requests
     # Calls find_handler() to get path and 
-    # handles it accordinly
+    # handles it accordingly
     def handle_request(self, request):
         response = Response()
 
@@ -49,4 +52,8 @@ class API:
         def wrapper(handler):
             self.routes[path] = handler
             return handler
+
         return wrapper
+
+    def encrypt_string(self, hash_string):
+        return hashlib.sha256(hash_string.encode()).hexdigest()
--- python/app.py
+++ python/app.py
@@ -1,22 +1,34 @@
 from api import API
+import os
+import hashlib
 from wsgiref.simple_server import make_server
 
 # Main file that runs the entire app
 app = API()
 
-# Route for '/home' path
-@app.route("/home")  # Flask-style annotation
+# Route for '/' path
+@app.route("/")  # Flask-style annotation
 def home(request, response):
-    response.text = "Hello from the HOME page"
+    response.text = open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+                         + '/index.html').read()
+    username = request.params.get('username', 'default')
+    passwd = app.encrypt_string(request.params.get('password', 'default'))
+    passwd = passwd[:32] # trims hash to 32 chars
+
+    # print(username + " " + passwd)
 
 # Route for '/success' path
-@app.route("/success")  # Flask-style annotation
+@app.route("/login")  # Flask-style annotation
 def success(request, response):
-    response.text = "Hello from the SUCCESS page"
+    response.text = open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+                         + '/success.html').read()
+
+
+
 
 # Test server for running the Python code
 httpd = make_server('localhost', 8000, app)
-    # print("Serving on port 8000...\nVisit http://127.0.0.1:8000\nTo kill the server we enter 'control + c'")
+print("Serving on port 8000...\nVisit http://127.0.0.1:8000\nTo kill the server we enter 'control + c'")
 
 # Wait for a single request, serve it and quit
 httpd.serve_forever()
