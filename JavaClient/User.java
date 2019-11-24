d8c42f3a6876e3fc273409caac49b3e5baaf05c0
Terlizzit <terlizzit@comcast.net>
2019-11-23 20:16:25 -0600

    Javadoc User

--- JavaClient/User.java
+++ JavaClient/User.java
@@ -1,3 +1,9 @@
+/**
+ * file:User.java
+ * author: Group 2 
+ * Date Modified: 11/23/19
+ * This class represents a MapZestUser
+ */
 package client;
 
 import java.nio.charset.Charset;
@@ -6,6 +12,11 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.math.BigInteger;
+
+/**
+ *@version 1.0
+ * Represents a MapZest User
+ */
 public class User {
 
     private static final Charset ENC = StandardCharsets.US_ASCII;
@@ -16,12 +27,21 @@ public class User {
     private String passwordHash;
     private Location loc;
 
+    /**
+     * Default Constructor
+     * Default User is given a location of 0,0
+     */
     public User(){
         isAuthenticated = false;
         friends = new ArrayList<String>();
         this.loc = new Location(0,0);
     }
 
+    /**
+     * Generate a password using the first 32 bytes of SHA-256
+     * @param pwd pwd to be generated
+     * @throws NoSuchAlgorithmException
+     */
     public void setPassword(String pwd) throws NoSuchAlgorithmException {
         MessageDigest md5 = MessageDigest.getInstance("SHA-256");
         byte[] digest = md5.digest(pwd.getBytes(ENC));
@@ -32,37 +52,74 @@ public class User {
         this.passwordHash = sb.toString();
     }
 
+    /**
+     * Default setter for email
+     * @param email Email to be set
+     */
     public void setEmail(String email){
         this.email = email;
     }
+
+    /**
+     * Gets the password
+     * @return hashed password
+     */
     public String getPassword(){
         return passwordHash;
     }
 
+    /**
+     * Sets authentication of the User
+     * @param isAuthenticated
+     */
     public void setAuhenticated(boolean isAuthenticated){
         this.isAuthenticated = isAuthenticated;
     }
 
+    /**
+     * Gets the authentication of the suer
+     * @return the authentication of the user
+     */
     public boolean getAuthenticated(){
         return isAuthenticated;
     }
 
+    /**
+     * Sets the location of the user
+     * @param loc Location of the user
+     */
     public void setLocation(Location loc){
         this.loc = loc;
     }
 
+    /**
+     * Gets the location of the user
+     * @return User's location
+     */
     public Location getLocation(){
         return loc;
     }
 
+    /**
+     * Sets the sign-in token of the user
+     * @param token sign-in token 
+     */
     public void setSignInToken(SignInToken token){
         this.signInToken = token;
     }
 
+    /**
+     * Gets the sign-in token
+     * @return sign-in token
+     */
     public SignInToken getToken(){
         return signInToken;
     }
 
+    /**
+     * Gets the email of the user
+     * @return The email of the user
+     */
     public String getEmail(){
         return email;
     }
