ab5cc592bbeec08bd033ea3026d41b1aeb8503aa
Connor Smith <marancps@gmail.com>
2019-11-26 01:05:45 -0600

    Added Database Stuff Added Redirects to Login/Register Error/Success messages

--- python/app.py
+++ python/app.py
@@ -9,8 +9,12 @@ from wsgiref.simple_server import make_server
 application = API()
 
 # Open db conn
-#conn = psycopg2.connect(database="cyberdb", user="postgres", password="postgres", host="localhost")
-#cur = conn.cursor()
+try:
+    conn = psycopg2.connect(database="cyberdb", user="postgres", password="postgres", host="localhost", port=5432)
+    conn.autocommit = True
+except:
+    print("Cannot connect to the database!")
+    exit(1)
 
 # Route for '/' path
 @application.route("/")  # Flask-style annotation
@@ -22,21 +26,49 @@ def home(request, response):
     # user clicks the submit button
     username = request.params.get('username', 'default')
     passwd = application.encrypt_string(request.params.get('password', 'default'))
-    passwd = passwd[:32] # trims hash to 32 chars
+    passwd = passwd[:32]  # trims hash to 32 chars
 
-    ## query goes here ##
-
-    #####################
-
-    ## compare results of query for valid login ##
-
-    ##############################################
-
-    # If valid
-    if username == 'darbym':
-        success(request, response)
+    if username != "default":  # This fixes the problem of redirecting back to a page and it instantly submits the form
+        # Check for valid username/password
+        query = "SELECT * FROM people WHERE email='{}' AND pwhash='{}'".format(username, passwd)
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(query)
+                if cursor.rowcount > 0:
+                    print("Username and Password match!")
+                    success(request, response)
+                else:
+                    print("Invalid Username or Password!")
+                    loginerror(response)
+                    # print('no redirect')
+        except psycopg2.Error as e:
+            print(e.pgerror)
     else:
-        print('no redirect')
+        print("Blank Username or Password on login")
+
+    # # If valid
+    # if username == 'darbym':
+    #     success(request, response)
+    # else:
+    #     print('no redirect')
+
+# Route for '/loginerror' path
+@application.route("/loginerror")  # Flask-style annotation
+def loginerror(response):
+    response.text = open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+                         + '/loginerror.html').read()
+
+# Route for '/registererror' path
+@application.route("/registererror")  # Flask-style annotation
+def registererror(response):
+    response.text = open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+                         + '/registererror.html').read()
+
+# Route for '/registersuccess' path
+@application.route("/registersuccess")  # Flask-style annotation
+def registersuccess(response):
+    response.text = open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+                         + '/registersuccess.html').read()
 
 # Route for '/success' path
 @application.route("/success")  # Flask-style annotation
@@ -52,17 +84,29 @@ def register(request, response):
     username = request.params.get('username', 'default')
     passwd = application.encrypt_string(request.params.get('password', 'default'))
     passwd = passwd[:32]
-    # cur.execute("INSERT INTO people (username)")
 
-    # con.commit()
-    # con.close()
+    # check for existing username
+    if username != "default":  # This fixes the problem of redirecting back to a page and it instantly submits the form
+        query = "SELECT * FROM people WHERE email='{}'".format(username)
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(query)
+                if cursor.rowcount > 0:
+                    print("Username already exists!")
+                    registererror(response)
+                else:
+                    query = "INSERT INTO people (email, pwhash) VALUES ('{}', '{}')".format(username, passwd)
+                    cursor.execute(query)
+                    registersuccess(response)
+        except psycopg2.Error:
+            print(psycopg2.Error)
+    else:
+        print("Blank Username or Password on Register")
 
 
-
-
-# Test server for running the Python code
-httpd = make_server('localhost', 8000, application)
-print("Serving on port 8000...\nVisit http://127.0.0.1:8000\nTo kill the server we enter 'control + c'")
-
-# Wait for a single request, serve it and quit
-httpd.serve_forever()
+if __name__ == "__main__":
+    # Test server for running the Python code
+    httpd = make_server('localhost', 8000, application)
+    print("Serving on port 8000...\nVisit http://127.0.0.1:8000\nTo kill the server we enter 'control + c'")
+    # Wait for a single request, serve it and quit
+    httpd.serve_forever()
